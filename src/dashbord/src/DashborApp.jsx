455da86036d0e9fd5fa3fc3d0fc87755830f6424
mrHong <hongseok.k@ingstory.kr>
2024-07-01 10:06:38 +0900

    리엑트 쿼리 주입

--- src/dashbord/src/DashborApp.jsx
+++ src/dashbord/src/DashborApp.jsx
@@ -1,15 +1,23 @@
 import {Outlet} from "react-router-dom";
 import Navigator from "../../common/component/Navigator.jsx";
 import DashbordStyled from "../../../style/DashbordStyled.jsx";
+import ErrorRender from "../../common/error/ErrorRender.jsx";
+import {ErrorBoundary} from "@suspensive/react";
+import {Suspense} from "react";
+import Loading from "../../common/loadingTemplate/Loading.jsx";
 
 function DashborApp() {
   return (
-      <DashbordStyled>
-        <Navigator className='navigator'/>
-          <div className='outlet_con'>
-              <Outlet/>
-          </div>
-      </DashbordStyled>
+      <ErrorBoundary FallbackComponent={ErrorRender}>
+          <Suspense fallback={<Loading />}>
+              <DashbordStyled>
+                <Navigator className='navigator'/>
+                  <div className='outlet_con'>
+                      <Outlet/>
+                  </div>
+              </DashbordStyled>
+          </Suspense>
+      </ErrorBoundary>
   )
 }
 
